0b18bda5db1b725c942e113982df07328e3c6576
Max1202 <melnikovmaks1202@gmail.com>
2024-03-18 12:54:40 +0200

    Add tests to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import SignIn from "./components/SignIn/SignIn";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders login/register text", () => {
+  render(<SignIn />);
+  const loginRegisterText = screen.getByText("Login/Register:");
+  expect(loginRegisterText).toBeInTheDocument();
+});
+
+test("renders register button", () => {
+  render(<SignIn />);
+  const registerButton = screen.getByText("Register");
+  expect(registerButton).toBeInTheDocument();
+});
+
+test("switches to login form when login button is clicked", () => {
+  render(<SignIn />);
+  const loginForm = screen.getAllByPlaceholderText(/Email*/i)[0];
+  expect(loginForm).toBeInTheDocument();
+});
+
+test("switches to register form when register button is clicked", () => {
+  render(<SignIn />);
+  const registerButton = screen.getByText("Register");
+  fireEvent.click(registerButton);
+  const registerForm = screen.getByText("Register Component");
+  expect(registerForm).toBeInTheDocument();
 });
